4638ff2f77caadf6696883023fe01adb1f18cd8e
Harshit <harshit.garg@togopool.com>
2025-02-14 13:00:33 +0530

    demo Code

---
+++ Sources/Orca Shuttle/HomeVC.swift
@@ -0,0 +1,24 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
