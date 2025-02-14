672c9f8555d04797b4fc9848959da51d8681538b
Harshit <harshit.garg@togopool.com>
2025-02-14 15:35:09 +0530

    chnage code to swift ui

---
+++ Sources/Orca Shuttle/Home.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView 2.swift
+//  Orca Shuttle
+//
+//  Created by Harshit Garg on 14/02/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView_2()
+}
