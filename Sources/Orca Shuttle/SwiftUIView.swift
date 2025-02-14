b8d99d2e609514508afa1284043466c559eae10f
Harshit <harshit.garg@togopool.com>
2025-02-14 16:40:29 +0530

    added public init

--- Sources/Orca Shuttle/SwiftUIView.swift
+++ Sources/Orca Shuttle/SwiftUIView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 public struct SwiftUIView: View {
+    public init() {}
     public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
