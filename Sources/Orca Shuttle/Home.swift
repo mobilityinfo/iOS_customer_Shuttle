78f30383cd905a6d1de7c7583d874716632fc460
Harshit <harshit.garg@togopool.com>
2025-02-14 15:36:44 +0530

    make it public

--- Sources/Orca Shuttle/Home.swift
+++ Sources/Orca Shuttle/Home.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct SwiftUIView_2: View {
+struct Home: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SwiftUIView_2()
+    Home()
 }
--- Sources/Orca Shuttle/SwiftUIView.swift
+++ Sources/Orca Shuttle/SwiftUIView.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
+public struct SwiftUIView: View {
+    public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
