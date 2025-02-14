1681f9e6aedcdcf034493d821d28c01f2346b69c
Harshit <harshit.garg@togopool.com>
2025-02-14 13:17:21 +0530

    data

--- Sources/Orca Shuttle/HomeShuttleVC.swift
+++ Sources/Orca Shuttle/HomeShuttleVC.swift
@@ -2,23 +2,12 @@
 // https://docs.swift.org/swift-book
 import UIKit
 
-class HomeShuttleVC: UIViewController {
+public class HomeShuttleVC: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
