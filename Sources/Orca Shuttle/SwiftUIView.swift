7d528a560f2ec32b4ddbfea6ddece38c277405a1
Harshit <harshit.garg@togopool.com>
2025-02-14 15:15:44 +0530

    swift ui file added

---
+++ Sources/Orca Shuttle/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Orca Shuttle
+//
+//  Created by Harshit Garg on 14/02/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
